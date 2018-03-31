9e5235d4611154282ddd0a371e40337399e9edb0
Giglium <fre095@gmail.com>
2018-03-31 12:08:28 +0200

    add autorefresh

--- html/assets/js/gui/common.js
+++ html/assets/js/gui/common.js
@@ -20,11 +20,6 @@ function populateSideBar() {
     });
 }
 
-function clearSideBar() {
-    $('#components').remove();
-    $('#component').remove();
-}
-
 function populateBell() {
     $.ajax({
         url: "/api/queue/?limit=3&pending=true",
@@ -39,22 +34,25 @@ function populateBell() {
     });
 }
 
-$(document).ready(function() {
-    populateSideBar();
-    //populateBell();
-    populateNotify();
-    setInterval(populateNotify, 5000);
+function populateNotify() {
+    $.ajax({
+        url: "/api/queue/count",
+        type: "GET",
+        success: function(data) {
+            if(data.length > 0)
+                $("#notify").text(data[0]['result']);
+            populateBell();
+        }
+    });
+}
 
-    function populateNotify() {
-        $.ajax({
-            url: "/api/queue/count",
-            type: "GET",
-            success: function(data) {
-                if(data.length > 0)
-                    $("#notify").text(data[0]['result']);
-                    populateBell();
-            }
-        });
+$(document).ready(function() {
+    refresh();
+    setInterval(refresh, 5000);
+    
+    function refresh() {
+        populateNotify();
+        populateSideBar();
     }
 
 });
