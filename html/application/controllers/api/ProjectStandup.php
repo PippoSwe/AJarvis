73dc0979b73f4664a81d985dfde106ce4f171df7
Mauro Fasolo <mr.yattle@gmail.com>
2018-03-31 10:39:28 +0200

    FIX: Mauro è un pò goldone

--- html/application/controllers/api/ProjectStandup.php
+++ html/application/controllers/api/ProjectStandup.php
@@ -301,12 +301,7 @@ class ProjectStandup extends CI_Controller
         $wav_file  = $path . '/' . $fname . ".wav";
         $flac_file = $path . '/' . $fname . ".FLAC";
 
-        if(!move_uploaded_file($_FILES['file']['tmp_name'], $wav_file)) {
-            echo "Non va zio canotto";
-            return FALSE;
-        }
-
-        //rename ( $_FILES['file']['tmp_name'], $wav_file );
+        rename ( $_FILES['file']['tmp_name'], $wav_file );
         // convert wav to FLAC
         $command = '/usr/bin/ffmpeg -i ' . $wav_file  . ' -ac 1 ' . $flac_file;
         exec($command);
