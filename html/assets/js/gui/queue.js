f2c222bf31cb78522152cc1f84e574ffd549362b
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-04-28 11:41:57 +0200

    queue

--- html/assets/js/gui/queue.js
+++ html/assets/js/gui/queue.js
@@ -9,13 +9,14 @@ $(document).ready(function() {
             url: "/api/queue/",
             type: "GET",
             success: function(data) {
-                if(data.length > 0)
+                if(data.length > 0){
                     $.get('item.mst', function(template) {
                         var rendered = Mustache.render(template, {items: data});
                         $('#list').html(rendered);
+                        $('[data-toggle="tooltip"]').tooltip();
                     });
+                }
             }
         });
     }
-
 });
